00d626e4e24deb9340a43345cbc22db130462f3a
Deepak-Codes2205 <deepak.contact122@gmail.com>
2026-05-27 00:45:25 +0530

    Updated Backend Server

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ import { testAi } from "./src/services/ai.service.js";
 const PORT = process.env.PORT || 8000;
 //console.log(process.env.MONGO_URI);
 
-testAi(); // We will test the AI service here, In production we will not this, In production this function will not be created
+//testAi(); // We will test the AI service here, In production we will not this, In production this function will not be created
 // Here it is just to check the AI service is working fine or not
 connectDB()
     .catch((err) => {
